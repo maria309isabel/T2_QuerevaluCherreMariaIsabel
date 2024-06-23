65afa6c96df157108f282d19f09bfa6bd843c90c
maria309isabel <isa.mar.xv@gmail.com>
2024-06-22 21:49:13 -0500

    Inicio pregunta 3

--- src/app/dashboard/pregunta3/pregunta3.component.ts
+++ src/app/dashboard/pregunta3/pregunta3.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-pregunta3',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './pregunta3.component.html',
   styleUrl: './pregunta3.component.css'
 })
