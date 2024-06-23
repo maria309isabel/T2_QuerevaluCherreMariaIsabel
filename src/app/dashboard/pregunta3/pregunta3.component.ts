a9d99b94caf54bb8676b9721b4b22377e7273e1e
maria309isabel <isa.mar.xv@gmail.com>
2024-06-22 21:52:16 -0500

    pregunta 3

--- src/app/dashboard/pregunta3/pregunta3.component.ts
+++ src/app/dashboard/pregunta3/pregunta3.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../angular-material/material/material.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pregunta3',
@@ -10,4 +11,37 @@ import { MaterialModule } from '../../angular-material/material/material.module'
 })
 export class Pregunta3Component {
 
+  alcoholForm: FormGroup;
+
+  vehicles = [
+    { type: 'Camión', indicator: 'C', maxRate: 0.3 },
+    { type: 'Autobús', indicator: 'A', maxRate: 0.3 },
+    { type: 'Turismo', indicator: 'T', maxRate: 0.5 },
+    { type: 'Motocicleta', indicator: 'M', maxRate: 0.3 }
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.alcoholForm = this.fb.group({
+      vehicleType: ['', Validators.required],
+      alcoholRate: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.alcoholForm.invalid) {
+      return;
+    }
+
+    const formValues = this.alcoholForm.value;
+    const vehicle = this.vehicles.find(v => v.indicator === formValues.vehicleType);
+
+    if (vehicle && formValues.alcoholRate > vehicle.maxRate) {
+      alert('Positivo en el control de alcoholemia');
+    } else {
+      alert('Negativo en el control de alcoholemia');
+    }
+  }
 }
+
+
+
